ddd3e60e22583938d22c520c479f0f43af671818
chadipirallamahesh <chadipirallamahesh@gmail.com>
2022-12-06 16:59:11 +0530

    J-Unit

---
+++ junit/EvenNumber.java
@@ -0,0 +1,13 @@
+package com.junit;
+
+public class EvenNumber {
+	static int number = 5;
+	public static boolean FindEvenOddNumber() {
+		if(number % 2 == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
